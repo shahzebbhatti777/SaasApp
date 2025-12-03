d528928f773a113f9fb02fcd0506284f15a40e30
shahzebbhatti777 <harryjames2293@gmail.com>
2025-12-04 00:03:04 +0500

    Improve auth token error responses

--- api/auth/auth.js
+++ api/auth/auth.js
@@ -5,12 +5,12 @@ function authenticateToken(req, res, next) {
   const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) {
-    return res.sendStatus(401);
+    return res.status(401).json({ error: 'Authorization token required' });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-      return res.sendStatus(403);
+      return res.status(403).json({ error: 'Invalid token' });
     }
 
     req.user = user;
